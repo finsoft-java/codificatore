28b970a72851f81d008afaa58f9bebec8ed7b2a2
emanuele_alesina94 <emanuele.alesina@certimeter.it>
2021-06-25 11:14:07 +0200

    Modifica pagina schemi-codifica

--- codifica/src/app/app-routing.module.ts
+++ codifica/src/app/app-routing.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './_guards/auth.guard';
-import { RegoleCodificaComponent } from './regole-codifica/regole-codifica.component';
-import { RegoleCodificaFormComponent } from './regole-codifica-form/regole-codifica-form.component';
+import { SchemiCodificaComponent } from './schemi-codifica/schemi-codifica.component';
+import { SchemiCodificaFormComponent } from './schemi-codifica-form/schemi-codifica-form.component';
 import { CodificaComponent } from './codifica/codifica.component';
 import { RicercaComponent } from './ricerca/ricerca.component';
 import { RicercaAvanzataComponent } from './ricerca-avanzata/ricerca-avanzata.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent},
-  { path: 'codifica', component: CodificaComponent, canActivate: [AuthGuard]},
-  { path: 'regole-codifica', component: RegoleCodificaComponent, canActivate: [AuthGuard]},
-  { path: 'regole-codifica/crea', component: RegoleCodificaFormComponent, canActivate: [AuthGuard]},
-  { path: 'regole-codifica/:id', component: RegoleCodificaFormComponent, canActivate: [AuthGuard]},
-  { path: 'ricerca', component: RicercaComponent, canActivate: [AuthGuard]},
-  { path: 'ricerca-avanzata', component: RicercaAvanzataComponent, canActivate: [AuthGuard]},
+  { path: 'login', component: LoginComponent },
+  { path: 'codifica', component: CodificaComponent, canActivate: [AuthGuard] },
+  { path: 'schemi-codifica', component: SchemiCodificaComponent, canActivate: [AuthGuard] },
+  { path: 'schemi-codifica/crea', component: SchemiCodificaFormComponent, canActivate: [AuthGuard] },
+  { path: 'schemi-codifica/:id', component: SchemiCodificaFormComponent, canActivate: [AuthGuard] },
+  { path: 'ricerca', component: RicercaComponent, canActivate: [AuthGuard] },
+  { path: 'ricerca-avanzata', component: RicercaAvanzataComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'codifica' }];
 
 @NgModule({
---
+++ codifica/src/app/schemi-codifica-form/schemi-codifica-form.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schemi-codifica-form',
+  templateUrl: './schemi-codifica-form.component.html',
+  styleUrls: ['./schemi-codifica-form.component.css']
+})
+export class SchemiCodificaFormComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
