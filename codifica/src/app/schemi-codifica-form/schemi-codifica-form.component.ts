96c8cf900574f3ba93fa12e2e68135d24901426f
emanuele_alesina94 <emanuele.alesina@certimeter.it>
2021-06-28 17:54:20 +0200

    modifiche a schemi-codifica-form

--- codifica/src/app/schemi-codifica-form/schemi-codifica-form.component.ts
+++ codifica/src/app/schemi-codifica-form/schemi-codifica-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { SchemaCodificaRegole } from '../_models';
+import { SchemiCodificaService } from '../_services/schemi-codifica.service';
+import { SchemiCodificaRegoleService } from '../_services/schemi-codifica-regole.service';
+import { AlertService } from '../_services/alert.service';
 
 @Component({
   selector: 'app-schemi-codifica-form',
@@ -10,12 +14,50 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class SchemiCodificaFormComponent implements OnInit {
   header: string = '';
   selectedSchemaType: string = '';
-  schemaTypeControl: FormControl = new FormControl('P');
-  schemiCodificaForm: FormGroup;
+  types: String[]= ['Tipo', 'Numero', 'Testo', 'Elenco', 'Sottoschema', 'Case'];
   currentSection: number = 1;
+  typeSelected: string ='Tipo';
+  elencoOptionsSelected: string = '';
+  sottoschemaOptionsSelected: string = '';
+  schemaCodificaForm = this.fb.group({
+    ID_SCHEMA: [-1, [Validators.required]],
+    TITOLO: ['', [Validators.required]],
+    DESCRIZIONE: '',
+    TIPOLOGIA: ['', [Validators.required]],
+    TPL_CODICE: ['', [Validators.required]],
+    TPL_DESCRIZIONE: ['', [Validators.required]],
+    PRE_RENDER_JS: ['', [Validators.required]],
+    IS_VALID: ['', [Validators.required]],
+    NOTE_INTERNE: ''
+  });
+  schemaCodificaRegole: SchemaCodificaRegole[]= [];
 
-  constructor(private route: ActivatedRoute, fb: FormBuilder) {
-    this.schemiCodificaForm = fb.group({ schemaTypeControl: this.schemaTypeControl, });
+  parsingRules = this.fb.group({
+    ID_SCHEMA: [-1, [Validators.required]],
+    NOM_VARIABILE: '',
+    ORD_PRESENTAZIONE: '',
+    ETICHETTA: '',
+    REQUIRED: '',
+    TIPO: 'Tipo',
+    MAXLENGTH: -1,
+    PATTERN_REGEXP: '',
+    NUM_DECIMALI: 0,
+    MIN: 0,
+    MAX: 0
+  });
+
+  get f1() { return this.schemaCodificaForm.controls; }
+
+  get f2() { return this.parsingRules.controls; }
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private alertService: AlertService,
+    public fb: FormBuilder,
+    public schemaCodificaService: SchemiCodificaService,
+    public schemiCodificaRegoleService: SchemiCodificaRegoleService)
+  {
   }
 
   ngOnInit(): void {
@@ -23,13 +65,29 @@ export class SchemiCodificaFormComponent implements OnInit {
       const { id } = params; // uguale a const id = params.id
       this.header = id ? 'Modifica Schema: ' + id : 'Nuovo Schema';
     });
+    this.getRegoleEsistenti();
   }
 
-  nextSection() {
-    this.currentSection += 1;
+  getRegoleEsistenti(): void {
+    this.schemiCodificaRegoleService.getAll()
+      .subscribe(
+        data => {
+          this.schemaCodificaRegole = data.data;
+          console.log(this.schemaCodificaRegole);
+        },
+        error => {
+          if (error.status === 401 || error.status === 403) {
+            this.alertService.error('Errore del Server');
+          } else {
+            // Ad esempio: Impossibile conettersi al server PHP
+            this.alertService.error(error);
+          }
+          //this.loading = false;
+        }
+      );
   }
 
-  lastSection() {
-    this.currentSection -= 1;
+  logForm(): void {
+    console.log(this.schemaCodificaForm.controls);
   }
 }
