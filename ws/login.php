e2cfd32f09d5b4c1bd5a2314ef9a3a8b81fbc672
Luca Vercelli <luca.vercelli@it-present.com>
2021-08-16 17:38:31 +0200

    ldap

--- ws/login.php
+++ ws/login.php
@@ -43,35 +43,13 @@ function check_and_load_user($username, $pwd) {
         $user->nome = 'User';
         $user->cognome = 'Finsoft';
         $user->email = 'alessandro.barsanti@it-present.com';
+        $user->ruolo = 'validatore';
         return $user;
     }
 
     // POI, proviamo su LDAP
-    $ldap = ldap_connect("ldap://osai.loc");
-    if (FALSE === $ldap) {
-        print_error(500, "Errore interno nella configurazione di Active Directory: " . AD_SERVER);
-    }
-    // We have to set this option for the version of Active Directory we are using.
-    ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3) or die('Unable to set LDAP protocol version');
-    ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0); // We need this for doing an LDAP search.
-    $ldaprdn = $username . "@" . AD_DOMAIN;
-    $bind = @ldap_bind($ldap, $ldaprdn, $pwd);
-    if ($bind) {
-        $filter="(&(SamAccountName=$username)" . AD_FILTER . ")";
-        $result = ldap_search($ldap, AD_BASE_DN, $filter);
-        ldap_sort($ldap, $result, "sn");
-        $info = ldap_get_entries($ldap, $result);
-        if (!isset($info[0])) {
-            print_error(403, "User not authorized");
-        }
-        $user =  new stdClass();
-        $user->nome_utente = $info[0]["samaccountname"][0];
-        $user->nome = $info[0]["sn"][0];
-        $user->cognome = $info[0]["givenname"][0];
-        $user->email = isset($info[0]["mail"]) ? $info[0]["mail"][0] : null;
-        @ldap_close($ldap);
-        return $user;
-    }
+    global $ldapManager;
+    return $ldapManager->login($username, $pwd);
 }
 
 
